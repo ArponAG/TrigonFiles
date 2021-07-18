02d677e89cbf803fa8aea6a23e5698c0155656db
ArponAG <34260294+ArponAG@users.noreply.github.com>
2021-07-18 20:29:03 +0600

    Add files via upload

---
+++ Changelog.lua
@@ -0,0 +1,25 @@
+[[--- Changelog 1.5.4 ---- ]]
+
+() Fixed bug on Injection Menu/PopUp
+() Added 'Changelog'
+
+[[--- Changelog 1.5.3 ---- ]]
+
+() Updated Custom theme ("OLD THEMES WILL NOT WORK")
+() Added a lot more fields to customize the UI ("prob not gonna add anything more")
+() Added support to have infinite theme at the same time!
+() Changed Injection method
+() Fixed few bug for Script Execute
+() Now you can hide the ScriptBox ('heck settings tab')
+() Custom UI comming soon!!
+
+[[--- Changelog 1.5.2 ---- ]]
+
+() Added Custom theme
+() Changed some stuff on injection
+() Added K R N L api
+() Fixed a lot of UI bugs
+() Jiren Fixed a lot of shit on the Tab system 
+
+
+
