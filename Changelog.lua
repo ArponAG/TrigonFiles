4d578d4572a4ac69dcb955f5c61f1a5b0a51a895
ArponAG <34260294+ArponAG@users.noreply.github.com>
2021-08-19 16:02:30 +0600

    Update Changelog.lua

--- Changelog.lua
+++ Changelog.lua
@@ -1,60 +1,13 @@
-[[--- Changelog 1.6.2 ---- ]]
-()Fixed Auto Inject
-()Added Saveinstance in Settings ('Use it aftr you inject Trigon')
-()Fixed some injection issues
+[[--- Changelog 2.0.0 ---- ]]
+() New UI!!!
+() NEW EVERYTHING!!!!
+() Trigon API IS NOW POWERFUL AF!!!
+() if there is any issue related to the UI please let ARPON AG KNOW
+() ENJOY!!!!!!
 
-[[--- Changelog 1.6.1 ---- ]]
-
-() Added Custom Synapse UI ('Check Settings')
-() Added Inject notification
-() Dont use hide scriptbox section, it will break the UI. I'll fix later.
-
-
-[[--- Changelog 1.6 ---- ]]
-
-()Updated for ROBLOX UPDATEE
-()Trigon is OP Now :D
-
-[[--- Changelog 1.5.7 ---- ]]
-
-() Made 'Script Browser' load faster.
-() Now you can load scripts from 'Script Browser' (Old scripts are not loadable).
-() Fixed a bug with close button.
-
-[[--- Changelog 1.5.6.1 ---- ]]
-
-() FIXED CRASH AFTER [ Verifying ] !!!
--- 1.5.6
-() New Logo, New Splash Screen!!!
-() Fixed some UI bugs (might''ve broken more lol, so please report any bug to Arpon AG!
-
-[[--- Changelog 1.5.5 ---- ]]
-
-() Fixed status bug on trigon, (the status would not update if you unload a theme using the checkbox)
-() Updated Trigon launcher to 2.0.7 (Now it looks more Sexy <3
-
-[[--- Changelog 1.5.4 ---- ]]
-
-() Fixed bug on Injection Menu/PopUp
-() Added 'Changelog'
-
-[[--- Changelog 1.5.3 ---- ]]
-
-() Updated Custom theme ("OLD THEMES WILL NOT WORK")
-() Added a lot more fields to customize the UI ("prob not gonna add anything more")
-() Added support to have infinite theme at the same time!
-() Changed Injection method.
-() Fixed few bug for Script Execute.
-() Now you can hide the ScriptBox ('heck settings tab')
-() Custom UI comming soon!!
-
-[[--- Changelog 1.5.2 ---- ]]
-
-() Added Custom theme.
-() Changed some stuff on injection.
-() Added K R N L api.
-() Fixed a lot of UI bugs.
-() Jiren Fixed a lot of shit on the Tab system .
-
-
+///-- Coming Soon --\\\
+  
+() Custom Theme
+() Custom UI (Snapse UI & Old Trigon UI)
+() Featured Script Section (Customizeable)
 
