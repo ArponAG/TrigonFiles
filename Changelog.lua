1df049fc414ccbeda8c5663455da6958ceee1e10
ArponAG <34260294+ArponAG@users.noreply.github.com>
2021-09-03 00:41:40 +0600

    Update Changelog.lua

--- Changelog.lua
+++ Changelog.lua
@@ -1,4 +1,11 @@
 [[--- Changelog 2.0.1b ---- ]]
+() Discord Server got deleted so I added a Discord Pop at the start-up ('will be removed after 1 week')
+() New ScriptHub dedsign
+() Added Featured Script Section on ScriptHub ('change it from Global to Featured')
+() Fixed few bugs on Keysystem
+() More comming soon...
+
+[[--- Changelog 2.0.1b ---- ]]
 () Fixed KRNL & Electron API for Arsenal and other games (Before it was freezing on inject)
 
 [[--- Changelog 2.0.1a ---- ]]
