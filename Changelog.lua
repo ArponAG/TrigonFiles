8f07c2ddccedf0379412697bb4aab461c197ff61
ArponAG <34260294+ArponAG@users.noreply.github.com>
2021-09-14 03:13:10 +0600

    Update Changelog.lua

--- Changelog.lua
+++ Changelog.lua
@@ -1,6 +1,7 @@
 [[--- Changelog 2.0.4 ---- ]]
 () Added Timer for keys ('you can see how much time you have left on the key')
 () Fixed Discord widget
+() Disabled Admin perm for now, Might have to add it later.
 () Fixed some other bugs
 
 [[--- Changelog 2.0.3 ---- ]]
