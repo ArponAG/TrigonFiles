0ec2185df98d14cba9b3e475e9fdaf62bbde583b
ArponAG <34260294+ArponAG@users.noreply.github.com>
2021-11-29 15:44:57 +0600

    Update Changelog.lua

--- Changelog.lua
+++ Changelog.lua
@@ -1,59 +1,9 @@
-[[--- Changelog 2.0.5 ---- ]]
-() Fixed Status (used to glitch after u clicked atattch button)
-() Reduced lag on script hub
-() Removed seconds from Key Status Timer
-
-[[--- Changelog 2.0.4e ---- ]]
-() Updated for Roblox Update!! [10/14/2021]
-
-[[--- Changelog 2.0.4a ---- ]]
-() Improved Multi API Status
-() Changed a lot of stuff related to Multi API, now its better :DD
-
-[[--- Changelog 2.0.4 ---- ]]
-() Added Timer for keys ('you can see how much time you have left on the key')
-() Fixed Discord widget
-() Disabled Admin perm for now, Might have to add it later.
-() Fixed some other bugs
-
-[[--- Changelog 2.0.3 ---- ]]
-() Remove Discord popup at start-up
-() ADDED CUSTOM THEME!!!!
-() Added Custom UI('SynapseUI')
-() Fixed some bugs
-() Might've added more bugs so please report if you find any :P
-
-
-[[--- Changelog 2.0.2 ---- ]]
-() Discord Server got deleted so I added a Discord Pop at the start-up ('will be removed after 1 week')
-() New ScriptHub dedsign
-() Added Featured Script Section on ScriptHub ('change it from Global to Featured')
-() Fixed few bugs on Keysystem
-() More comming soon...
-
-[[--- Changelog 2.0.1b ---- ]]
-() Fixed KRNL & Electron API for Arsenal and other games (Before it was freezing on inject)
-
-[[--- Changelog 2.0.1a ---- ]]
-() Removed Inject Notification for security reasons, But it still prints in the console. 
-() Fixed Execute button on Script List
-() Fixed some bug on Get key!
-() Fixed some other internal bugs
-
-[[--- Changelog 2.0.1 ---- ]]
-() Fixed Invalid Key problem
-() Fixed Language problem!
-
-[[--- Changelog 2.0.0 ---- ]]
-() New UI!!!
-() NEW EVERYTHING!!!!
-() Trigon API IS NOW POWERFUL AF!!!
-() if there is any issue related to the UI please let ARPON AG KNOW
-() ENJOY!!!!!!
-
-///-- Coming Soon --\\\
-  
-() Custom Theme
-() Custom UI (Snapse UI & Old Trigon UI)
-() Featured Script Section (Customizeable)
-
+[[--- Changelog 2.1 ---- ]]
+() Rerquire Admin Permission (for the new injector)
+() New Injector ('Roblox anti-cheat bypassed')
+() 268 Error Fix
+() Fixed tabs not saving or saving emtpy tabs
+() Fixed lag issue on Script-Hub
+() Added some new stuff to the KeyPage
+() Cleaned the Trigon folder
+() Finally cleaned up some codes (:
